b7ede905551ef6aa287a853b9f4f86bce9180aa5
Arthur <arthur.heynard@concoursmania.com>
2013-01-04 16:29:06 +0100

    premier comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamplApp::Application.config.secret_token = '48786f63a19e28339ba73c3135476d69edf9abce58666bb8ae9b2ff2ea1bd49b736788d29bc0602f659adf1fb970c384f67b1228735ac6f2866bf40cb85c306b'
